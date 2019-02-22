ef14c1d0ec4d80891221e7a1b0b86566a2fe6d3b
franksh <frnkschlssr@gmail.com>
2019-02-22 16:30:49 +0100

    recommit building functionality, closes #5, #6

--- src/sprites/buildings/Building.js
+++ src/sprites/buildings/Building.js
@@ -15,17 +15,5 @@ export default class Building extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
 
         this.alive = true;
-
-        // start still and wait until needed
-        // this.body
-        //     .setVelocity(0, 0)
-        //     .setBounce(0, 0)
-        //     .setCollideWorldBounds(false);
-        // this.body.allowGravity = false;
-        // this.beenSeen = false;
-
-        // Standard sprite is 16x16 pixels with a smaller body
-        // this.body.setSize(12, 12);
-        // this.body.offset.set(10, 12);
     }
 }
